fc4b5b9cdf7ffe8556cb9aaa6bc0c7de373cee67
miralda <miraldalexandra15@gmail.com>
2020-11-17 08:25:08 +0100

    add editors to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Editors from './components/Editors';
 import './style/app.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Apps</h1>
+      <h1>Codepen Clone - React App</h1>
+      <Editors />
     </div>
   );
 }
